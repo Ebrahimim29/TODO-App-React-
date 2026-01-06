201c0b611d10f18c12d595a80e6fb98fa5f4878e
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-01-06 22:38:47 +0330

    PureComponent-CreateRef

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,40 @@ import { useState } from "react";
 import TaskItems from "./TaskItems";
 import TopForm from "./TopForm";
 import { TaskContext } from "./TaskContext";
+import ParentCompo from "./Component/ParentComponent";
 
 function App() {
   const [taskItems, setTaskItems] = useState([
     {
       id: 1,
       title: "کار شماره 1",
-      done:false,
+      done: false,
     },
     {
       id: 4,
       title: "کار شماره 2",
-      done:true,
+      done: true,
     },
     {
       id: 3,
       title: "کار شماره 3",
-      done:false,
+      done: false,
     },
   ]);
 
   return (
-    <div className="container w-100 h-100 p-3">
-      <div className="row h-100 justify-content-center align-items-start">
-        <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-          <TaskContext.Provider value={{taskItems, setTaskItems}}>
-            <TopForm />
-            <TaskItems />
-          </TaskContext.Provider>
+    <div>
+      <div className="container w-100 h-100 p-3">
+        <div className="row h-100 justify-content-center align-items-start">
+          <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
+            <TaskContext.Provider value={{ taskItems, setTaskItems }}>
+              <TopForm />
+              <TaskItems />
+            </TaskContext.Provider>
+          </div>
         </div>
       </div>
+      <ParentCompo />
     </div>
   );
 }
